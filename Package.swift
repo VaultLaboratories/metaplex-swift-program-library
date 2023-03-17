2cc4836cf080919c9d0445da3368849d684594f3
Andy Boyd <andrew.boyd.ni@gmail.com>
2023-03-17 11:22:34 +0000

    point at fork

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/metaplex-foundation/beet-swift", branch: "1.0.7"),
-        .package(url: "https://github.com/metaplex-foundation/solita-swift.git", branch: "1.0.3")
+        .package(url: "https://github.com/VaultLaboratories/solita-swift.git", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
