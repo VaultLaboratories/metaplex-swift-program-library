2fe8f80923378b1d277ee28af5c5dc4541d3e32a
Andy Boyd <andrew.boyd.ni@gmail.com>
2023-03-17 13:05:55 +0000

    fixing package resolution woes

--- Package.swift
+++ Package.swift
@@ -25,7 +25,8 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/metaplex-foundation/beet-swift", branch: "1.0.7"),
-        .package(url: "https://github.com/VaultLaboratories/solita-swift.git", branch: "main")
+        .package(url: "https://github.com/VaultLaboratories/solita-swift.git", branch: "main"),
+        .package(name: "Solana", url: "https://github.com/VaultLaboratories/Solana.Swift.git", branch: "master")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
